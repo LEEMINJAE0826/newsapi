628cbdd9c83f02780dbdd51cd6b73a1daafa64b8
minjae lee <minjae1543@gmail.com>
2025-02-25 23:23:50 +0900

    add new code

---
+++ main.js
@@ -0,0 +1,14 @@
+const API_KEY = `230f1fb4e9f84766874e11aa6aa765d0`;
+let news = [];
+
+const getLatesNews = async () => {
+  const url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${8}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  news = data.articles;
+  console.log("data", news);
+};
+
+getLatesNews();
