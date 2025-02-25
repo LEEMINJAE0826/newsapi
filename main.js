8daf0e3f19cb779824ae06a7acfbe947ab057905
minjae lee <minjae1543@gmail.com>
2025-02-25 23:26:59 +0900

    add new code

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let news = [];
 
 const getLatesNews = async () => {
   const url = new URL(
-    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${8}`
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${8}` //누나스터디용 url 세팅
   );
   const response = await fetch(url);
   const data = await response.json();
