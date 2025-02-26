ab9b05c3a32763afcad93fd6f2de22084a6a0f76
MINJAE <minjae1543@gmail.com>
2025-02-26 19:35:19 +0900

    first commit

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 const API_KEY = `230f1fb4e9f84766874e11aa6aa765d0`;
-let news = [];
+let newsList = [];
 
 const getLatesNews = async () => {
   const url = new URL(
-    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${8}` //누나스터디용 url 세팅
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    // `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${8}` //누나스터디용 url 세팅
   );
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("data", news);
+  newsList = data.articles;
+  render();
+  console.log("data", newsList);
 };
 
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
+          <div class="col-lg-4">
+            <img
+              class="news-img-size"
+              src=${news.urlToImage}
+            />
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>${news.description}</p>
+            <div>${news.source.name} * ${news.publishedAt}</div>
+          </div>
+        </div>`
+    )
+    .join("");
+
+  document.getElementById("news-board").innerHTML = newsHTML;
+};
 getLatesNews();
